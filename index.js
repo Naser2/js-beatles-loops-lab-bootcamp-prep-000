b93e6df63f0541a28b1b70b982f6014e7fe2e43f
Nasser Sanou <nassersanou23@gmail.com>
2018-09-16 19:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 
 function johnLennonFacts(facts) {
 var  i = 0
-  while (i < facts.length;  i++ ) {
+
+  while (i < facts.length) {
+    i +=1 
     return facts[i] + '!'
     
   }
+   
    function iLoveTheBeatles(n) {
     var store = [] 
    }
  
 }
+ do {
+   
+ }
+while () {
+   
+ }
